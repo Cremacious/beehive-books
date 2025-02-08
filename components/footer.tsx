dc63ab8251be8ad25e7ce34589c67f9db23516e9
Cremacious <108367916+Cremacious@users.noreply.github.com>
2025-02-07 21:26:26 -0500

    Init

---
+++ components/footer.tsx
@@ -0,0 +1,14 @@
+import { APP_NAME } from '@/lib/constants';
+
+const Footer = () => {
+  const currentYear = new Date().getFullYear();
+  return (
+    <footer className="border-t bg-bee-dark ">
+      <div className="text-center p-5 flex-center text-bee-yellow">
+        {APP_NAME} &copy; {currentYear}. All Rights Reserved
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
