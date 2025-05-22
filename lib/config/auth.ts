b3a88f3347f551e973cab39e0287f7e6c81ee9a4
Cremacious <chrismackall3@gmail.com>
2025-05-22 15:50:57 -0400

    Setup nextjs with prisma

--- lib/config/auth.ts
+++ lib/config/auth.ts
@@ -1,5 +1,27 @@
 import NextAuth from 'next-auth';
+import Credentials from 'next-auth/providers/credentials';
+import { PrismaAdapter } from '@auth/prisma-adapter';
+import { prisma } from '@/lib/config/prisma';
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  providers: [],
+  adapter: PrismaAdapter(prisma),
+  pages: {
+    signIn: '/sign-in',
+    error: '/',
+  },
+  session: {
+    strategy: 'jwt' as const,
+    maxAge: 30 * 24 * 60 * 60,
+  },
+  providers: [
+    Credentials({
+      credentials: {
+        username: { label: 'Username' },
+        password: { label: 'Password', type: 'password' },
+      },
+      //   async authorize(credentials) {
+
+      //   },
+    }),
+  ],
 });
