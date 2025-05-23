7874f078fe9506b7fbfd8654112a986a948b2e1a
Cremacious <chrismackall3@gmail.com>
2025-05-23 10:12:08 -0400

    Removed unused import

--- app/(root)/signoutButton.tsx
+++ app/(root)/signoutButton.tsx
@@ -1,10 +1,10 @@
 'use client';
 import { Button } from '@/components/ui/button';
-import { signOutUser } from '@/lib/actions/user.actions';
+// import { signOutUser } from '@/lib/actions/user.actions';
 
 const SignOutButton = () => {
   const handleSignOut = async () => {
-    await signOutUser();
+    // await signOutUser();
     console.log('User signed out successfully');
   };
 
