9513dd582c35fa8b6d981a742638d0d1fcc830fb
Cremacious <chrismackall3@gmail.com>
2025-05-27 18:44:32 -0400

    Added working chapter page

--- app/(root)/book/[bookId]/[chapterId]/page.tsx
+++ app/(root)/book/[bookId]/[chapterId]/page.tsx
@@ -13,7 +13,30 @@ const ChapterPage = async (params: {
     );
   }
 
-  return <>{chapter.title}</>;
+  return (
+    <div className="container mx-auto justify-center space-y-4">
+      <div className="newCard p-4 max-w-lg mx-auto">
+        <div className="text-3xl text-center text-beeYellow font-bold">
+          {chapter.title}
+        </div>
+      </div>
+
+      <div className="newCard p-8 max-w-7xl mx-auto">
+        <div className="text-2xl text-center text-white">{chapter.content}</div>
+      </div>
+      <div className="newCard p-4 max-w-7xl mx-auto">
+        <div className="text-2xl text-center text-beeYellow font-bold">
+          Author&apos;s Notes
+        </div>
+        {chapter.notes}
+      </div>
+      <div className="newCard p-4 max-w-7xl mx-auto">
+        <div className="text-2xl text-center text-beeYellow font-bold">
+          Comments
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default ChapterPage;
