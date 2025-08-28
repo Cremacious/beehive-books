439c1249b8ba1391f7f710d7498ec7f915f2a757
cremacious <chrismackall3@gmail.com>
2025-08-28 12:13:41 -0400

    Sidebar responsive to user sign in

--- src/components/header/sidebar.tsx
+++ src/components/header/sidebar.tsx
@@ -61,71 +61,102 @@ export function Sidebar() {
             Beehive Books
           </SheetTitle>
         </SheetHeader>
-        <div className="space-y-4 mx-2 mt-6">
-          <div className="flex justify-center mb-4">
-            <Image
-              src={imageSrc ?? defaultProfileImage}
-              alt="Profile"
-              width={60}
-              height={60}
-              className="rounded-full border-2 border-yellow-400 object-cover"
-              style={{ aspectRatio: '1 / 1' }}
-              priority
-              onClick={() => router.push(`/profile/${session?.user?.id}`)}
-            />
-          </div>
-          <div className="grid grid-cols-2 gap-4">
-            <Button
-              variant="ghost"
-              className="relative text-xl font-bold w-full text-center px-4 py-8 text-yellow-300 rounded-lg overflow-hidden transition-all duration-300 group flex flex-col items-center"
-            >
-              <div className="absolute inset-0 bg-gradient-to-r from-white/10 to-white/20 opacity-60 group-hover:opacity-80 transition-opacity duration-300"></div>
-              <div className="absolute inset-0 backdrop-blur-sm border border-white/20 group-hover:border-white/40 rounded-lg transition-all duration-300"></div>
-              <Badge className="relative z-0 bg-yellow-400 text-slate-800 text-xs font-bold rounded-full px-2 py-0.5 border-2 border-black">
-                {unreadMessages}
-              </Badge>
-              <span className="relative z-10 text-sm">Unread Messages</span>
-            </Button>
-            <Button
-              variant="ghost"
-              className="relative text-xl font-bold w-full text-center px-4 py-8 text-yellow-300 rounded-lg overflow-hidden transition-all duration-300 group flex flex-col items-center"
-            >
-              <div className="absolute inset-0 bg-gradient-to-r from-white/10 to-white/20 opacity-60 group-hover:opacity-80 transition-opacity duration-300"></div>
-              <div className="absolute inset-0 backdrop-blur-sm border border-white/20 group-hover:border-white/40 rounded-lg transition-all duration-300"></div>
-              <Badge className="relative z-10 bg-yellow-400 text-slate-800 text-xs font-bold rounded-full px-2 py-0.5 border-2 border-black">
-                {pendingFriendRequests}
-              </Badge>
-              <span className="relative z-10 text-sm">Friend Requests</span>
-            </Button>
-          </div>
 
-          {ROUTES.map((route) => (
+        {session?.user ? (
+          <div className="space-y-4 mx-2 mt-6">
+            <div className="flex justify-center mb-4">
+              <Image
+                src={imageSrc ?? defaultProfileImage}
+                alt="Profile"
+                width={60}
+                height={60}
+                className="rounded-full border-2 border-yellow-400 object-cover"
+                style={{ aspectRatio: '1 / 1' }}
+                priority
+                onClick={() => router.push(`/profile/${session?.user?.id}`)}
+              />
+            </div>
+            <div className="grid grid-cols-2 gap-4">
+              <Button
+                variant="ghost"
+                className="relative text-xl font-bold w-full text-center px-4 py-8 text-yellow-300 rounded-lg overflow-hidden transition-all duration-300 group flex flex-col items-center"
+              >
+                <div className="absolute inset-0 bg-gradient-to-r from-white/10 to-white/20 opacity-60 group-hover:opacity-80 transition-opacity duration-300"></div>
+                <div className="absolute inset-0  border border-white/20 group-hover:border-white/40 rounded-lg transition-all duration-300"></div>
+                <Badge className="relative z-0 bg-yellow-400 text-slate-800 text-xs font-bold rounded-full px-2 py-0.5 border-2 border-black">
+                  {unreadMessages}
+                </Badge>
+                <span className="relative z-10 text-sm">Unread Messages</span>
+              </Button>
+              <Button
+                variant="ghost"
+                className="relative text-xl font-bold w-full text-center px-4 py-8 text-yellow-300 rounded-lg overflow-hidden transition-all duration-300 group flex flex-col items-center"
+              >
+                <div className="absolute inset-0 bg-gradient-to-r from-white/10 to-white/20 opacity-60 group-hover:opacity-80 transition-opacity duration-300"></div>
+                <div className="absolute inset-0  border border-white/20 group-hover:border-white/40 rounded-lg transition-all duration-300"></div>
+                <Badge className="relative z-10 bg-yellow-400 text-slate-800 text-xs font-bold rounded-full px-2 py-0.5 border-2 border-black">
+                  {pendingFriendRequests}
+                </Badge>
+                <span className="relative z-10 text-sm">Friend Requests</span>
+              </Button>
+            </div>
+
+            {ROUTES.map((route) => (
+              <Button
+                key={route.name}
+                variant="ghost"
+                onClick={() => handleButtonClick(route.path)}
+                className="text-xl font-bold w-full text-center px-4 py-6 text-yellow-300  relative overflow-hidden rounded-lg transition-all duration-300 group"
+              >
+                <div className="absolute inset-0 bg-gradient-to-r from-white/10 to-white/20 opacity-60 group-hover:opacity-80 transition-opacity duration-300"></div>
+
+                <div className="absolute inset-0 border border-white/20 group-hover:border-white/40 rounded-lg transition-all duration-300"></div>
+
+                <span className="relative z-10">{route.name}</span>
+              </Button>
+            ))}
+          </div>
+        ) : (
+          <div className="space-y-4 mx-2 mt-6">
             <Button
-              key={route.name}
               variant="ghost"
-              onClick={() => handleButtonClick(route.path)}
+              onClick={() => router.push('/sign-in')}
               className="text-xl font-bold w-full text-center px-4 py-6 text-yellow-300  relative overflow-hidden rounded-lg transition-all duration-300 group"
             >
               <div className="absolute inset-0 bg-gradient-to-r from-white/10 to-white/20 opacity-60 group-hover:opacity-80 transition-opacity duration-300"></div>
 
-              <div className="absolute inset-0 backdrop-blur-sm border border-white/20 group-hover:border-white/40 rounded-lg transition-all duration-300"></div>
+              <div className="absolute inset-0 border border-white/20 group-hover:border-white/40 rounded-lg transition-all duration-300"></div>
 
-              <span className="relative z-10">{route.name}</span>
+              <span className="relative z-10">Sign In</span>
             </Button>
-          ))}
-        </div>
-        <SheetFooter>
-          <div className="flex justify-between items-center w-full">
-            <Settings
-              size={35}
-              className="text-yellow-400 hover:text-yellow-500 hoverAnimateTiny"
-              onClick={() => router.push('/settings')}
-            />
-            <Button onClick={handleSignOut} className="w-3/4" type="submit">
-              Sign Out
+            <Button
+              variant="ghost"
+              onClick={() => router.push('/sign-up')}
+              className="text-xl font-bold w-full text-center px-4 py-6 text-yellow-300  relative overflow-hidden rounded-lg transition-all duration-300 group"
+            >
+              <div className="absolute inset-0 bg-gradient-to-r from-white/10 to-white/20 opacity-60 group-hover:opacity-80 transition-opacity duration-300"></div>
+
+              <div className="absolute inset-0 border border-white/20 group-hover:border-white/40 rounded-lg transition-all duration-300"></div>
+
+              <span className="relative z-10">Sign Up</span>
             </Button>
           </div>
-        </SheetFooter>
+        )}
+    
+        {session?.user && (
+          <SheetFooter>
+            <div className="flex justify-between items-center w-full">
+              <Settings
+                size={35}
+                className="text-yellow-400 hover:text-yellow-500 hoverAnimateTiny"
+                onClick={() => router.push('/settings')}
+              />
+              <Button onClick={handleSignOut} className="w-3/4" type="submit">
+                Sign Out
+              </Button>
+            </div>
+          </SheetFooter>
+        )}
       </SheetContent>
     </Sheet>
   );
