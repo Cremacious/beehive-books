2f3cd68793f3386a21ec8637b7294488d7ab86b9
cremacious <chrismackall3@gmail.com>
2025-08-28 09:41:06 -0400

    Moved content provider to user root

--- src/app/(user)/messages/[messageId]/page.tsx
+++ src/app/(user)/messages/[messageId]/page.tsx
@@ -1,6 +1,7 @@
 import { getMessageById } from '@/lib/actions/message.actions';
-import { ArrowLeft } from 'lucide-react';
+import { MoveLeft } from 'lucide-react';
 import Link from 'next/link';
+import { Button } from '@/components/ui/button';
 
 export default async function MessagePage({
   params,
@@ -12,17 +13,17 @@ export default async function MessagePage({
 
   if (message.type === 'friend_request') {
     return (
-      <div className="mx-auto max-w-5xl px-2">
+      <div className="mx-auto max-w-5xl px-2 flex flex-col justify-center">
+        <div className="mb-4">
+          <Button variant={'secondary'} asChild>
+            <Link href="/messages">
+              <MoveLeft className="mr-2" />
+              Back to Messages
+            </Link>
+          </Button>
+        </div>
         <div className="darkContainer ">
           <div className="lightContainer p-6 md:p-10">
-            <div className="mb-6 flex items-center gap-2">
-              <Link
-                href="/messages"
-                className="inline-flex items-center text-yellow-700 hover:underline font-medium"
-              >
-                <ArrowLeft className="w-5 h-5 mr-1" /> Back to Messages
-              </Link>
-            </div>
             <div className="text-lg text-white mb-2">
               {message.sender} sent you a friend request!
             </div>
@@ -34,7 +35,11 @@ export default async function MessagePage({
             <div className="flex flex-col md:flex-row gap-4 md:gap-8 text-white text-sm">
               <div>
                 <span className="font-semibold text-yellow-400">Date:</span>{' '}
-                {message.date}
+                {new Date(message.date).toLocaleDateString(undefined, {
+                  month: '2-digit',
+                  day: '2-digit',
+                  year: '2-digit',
+                })}
               </div>
             </div>
           </div>
@@ -45,16 +50,16 @@ export default async function MessagePage({
 
   return (
     <div className="mx-auto max-w-5xl px-2">
+      <div className="mb-4">
+        <Button variant={'secondary'} asChild>
+          <Link href="/messages">
+            <MoveLeft className="mr-2" />
+            Back to Messages
+          </Link>
+        </Button>
+      </div>
       <div className="darkContainer ">
         <div className="lightContainer p-6 md:p-10">
-          <div className="mb-6 flex items-center gap-2">
-            <Link
-              href="/messages"
-              className="inline-flex items-center text-yellow-700 hover:underline font-medium"
-            >
-              <ArrowLeft className="w-5 h-5 mr-1" /> Back to Messages
-            </Link>
-          </div>
           <div className="text-lg text-white mb-2">
             <span className="text-yellow-400">{message.sender}</span> commented
             on <span className="text-yellow-400">{message.chapterTitle}</span>{' '}
@@ -67,7 +72,11 @@ export default async function MessagePage({
           <div className="flex flex-col md:flex-row gap-4 md:gap-8 text-white text-sm">
             <div>
               <span className="font-semibold text-yellow-400">Date:</span>{' '}
-              {message.date}
+              {new Date(message.date).toLocaleDateString(undefined, {
+                month: '2-digit',
+                day: '2-digit',
+                year: '2-digit',
+              })}
             </div>
           </div>
         </div>
