94c451baa860cb172461eafa1f05aa48e53b1a7d
ChrisMackall <chrismackall3@gmail.com>
2026-03-09 18:29:44 -0400

    Added todos

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,19 @@ import { Comfortaa } from 'next/font/google';
 import { Providers } from '@/app/providers';
 import './globals.css';
 
+//TODO:  Create alert on club page when pending join requests
+//TODO: Delete Dialog for deleting club discussion 
+//TODO: User's names can be clicked to view their profile
+//TODO: Fix issue with page shifting when uploading docx
+//TODO: Create notification page
+//TODO: Create settings page
+//TODO: Create loader for user image/name
+
+
+
+
+//TODO: Notifications work when receiving a request to join a club, when someone comments on your discussion, when someone posts a discussion in your club, NOT when someone likes your discussion comment, NOT when someone replies to your discussion comment, when someone posts in your prompt, when someone comments on your prompt entry, when someone  comments on your book, when someone replies to your book comment, when someone likes your book comment, when someone wants to join your hive, 
+
 const comfortaa = Comfortaa({
   subsets: ['latin'],
   weight: ['300', '400', '500', '600', '700'],
